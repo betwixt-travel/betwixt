36988f9469c60de81cd3415eb33ce19fdb88c3fe
m-ghiringhelli <mghiring@gmail.com>
2022-05-27 14:28:11 -0700

    create auth functions

--- src/services/user.js
+++ src/services/user.js
@@ -1,3 +1,29 @@
+import { client } from './client';
+
 export function getUser() {
-  
+  return client.auth.user();
+}
+
+export function getSession() {
+  return client.auth.session();
+}
+
+function handleError({ user, error }) {
+  if (error) throw error;
+  return user;
+}
+
+export async function signInUser(email, password) {
+  const response = await client.auth.signIn({ email, password });
+  return handleError(response);
+}
+
+export async function signUpUser(email, password) {
+  const response = await client.auth.signUp({ email, password });
+  return handleError(response);
+}
+
+export async function signOut() {
+  const response = await client.auth.signOut();
+  handleError(response);
 }
