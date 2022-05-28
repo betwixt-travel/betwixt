2c908be855b1fc5d624bdbf1d8cd6cb9b03990a0
aidanliddiard <aidanliddiard@gmail.com>
2022-05-28 13:24:34 -0700

    formating the header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import styles from './Header.css'
+import styles from './Header.css';
 
 export default function Header() {
   return (
