9eaf523ef86b5c9a5efe6269c943db0b73323fd5
Mary Martinez <mary.diana.martinez@gmail.com>
2022-05-31 16:39:01 -0700

    latitude and longitude are successfully obtained from params

--- src/views/Results.jsx
+++ src/views/Results.jsx
@@ -12,7 +12,9 @@ export default function Results() {
         <ul>
           {cities.map((city) => (
             <li key={city.properties.id}>
-              <Link to={`/results/lat=${city.properties.latitude}&long=${city.properties.longitude}`}>
+              <Link
+                to={`/results/${city.properties.latitude}+${city.properties.longitude}`}
+              >
                 {city.properties.name} - {city.properties.distance}
               </Link>
             </li>
--- src/views/ResultsDetail.jsx
+++ src/views/ResultsDetail.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 export default function ResultsDetail() {
@@ -6,13 +6,17 @@ export default function ResultsDetail() {
   const [lat, setLat] = useState();
   const [long, setLong] = useState();
 
-  // useEffect(() => {
+  useEffect(() => {
+    const coordinates = city.split('+');
+    setLat(coordinates[0]);
+    setLong(coordinates[1]);
     
-  // }, [])
+  }, [city]);
 
   return (
     <>
-      
+      <p>lat: {lat}</p>
+      <p>long: {long}</p>
     </>
-  )
+  );
 }
