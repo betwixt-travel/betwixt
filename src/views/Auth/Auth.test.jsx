ba0caa35253c27df90a4b2f91f380d88085b6d33
Denver McCarthy <dmichaelspdx@gmail.com>
2022-05-31 10:22:09 -0700

    changed msw urls to env path

--- src/views/Auth/Auth.test.jsx
+++ src/views/Auth/Auth.test.jsx
@@ -14,19 +14,19 @@ import { mockedUser, profileResponse } from '../../tests/fixtures/mockdata';
 
 const server = setupServer(
   rest.post(
-    'https://kyyzdtelzuxgssnlsorf.supabase.co/auth/v1/token',
+    `${process.env.REACT_APP_SUPABASE_URL}/auth/v1/token`,
     (req, res, ctx) => {
       return res(ctx.json(mockedUser));
     }
   ),
   rest.post(
-    'https://kyyzdtelzuxgssnlsorf.supabase.co/auth/v1/signup',
+    `${process.env.REACT_APP_SUPABASE_URL}/auth/v1/signup`,
     (req, res, ctx) => {
       return res(ctx.json(mockedUser));
     }
   ),
   rest.patch(
-    'https://kyyzdtelzuxgssnlsorf.supabase.co/rest/v1/profiles',
+    `${process.env.REACT_APP_SUPABASE_URL}/rest/v1/profiles`,
     (req, res, ctx) => {
       return res(ctx.json(profileResponse));
     }
