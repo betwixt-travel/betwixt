77911ebd66c6b6726e32576a4c097566bbdb71fc
aidanliddiard <aidanliddiard@gmail.com>
2022-06-01 14:53:43 -0700

    unsplash images displaying on the page and fixed refresh with new route

---
+++ src/services/images.js
@@ -0,0 +1,10 @@
+const API_KEY = process.env.REACT_APP_UNSPLASH_API_KEY;
+
+export async function fetchImages(city) {
+  const data = await fetch(
+    `https://api.unsplash.com/search/photos?page=1&query=${city}&order_by=popular&client_id=${API_KEY}`
+  );
+  const resp = await data.json();
+  console.log(resp);
+  return resp;
+}
--- src/views/ResultsDetail.jsx
+++ src/views/ResultsDetail.jsx
@@ -1,41 +1,75 @@
 import React, { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 import { useTravelContext } from '../context/TravelContext';
+import { fetchImages } from '../services/images';
 import { fetchCity } from '../services/places';
+import Styles from '../views/ResultsDetails.css';
 
 export default function ResultsDetail() {
-  const { city } = useParams();
+  const location = useLocation();
+  const params = new URLSearchParams(location.search);
+  const lat = params.get('lat');
+  const long = params.get('long');
   const { saveHandler } = useTravelContext();
-  const [lat, setLat] = useState();
-  const [long, setLong] = useState();
+  // const [lat, setLat] = useState();
+  // const [long, setLong] = useState();
   const [cityInfo, setCityInfo] = useState();
   const [loading, setLoading] = useState(true);
+  const [images, setImages] = useState([]);
 
   useEffect(() => {
-    const coordinates = city.split('+');
-    setLat(coordinates[0]);
-    setLong(coordinates[1]);
+    // const coordinates = city.split('+');
+    // setLat(coordinates[0]);
+    // setLong(coordinates[1]);
     const getCity = async () => {
       const cityData = await fetchCity({
-        lat: coordinates[0],
-        long: coordinates[1],
+        lat,
+        long,
       });
       setCityInfo(cityData[0]);
-      setLoading(false);
     };
     getCity();
-  }, []);
+  }, [lat, long]);
+
+  useEffect(() => {
+    if (!cityInfo) return;
+    const fetchImagesData = async () => {
+      const search = `${cityInfo.city}-${cityInfo.region}`;
+      const results = await fetchImages(search);
+      setImages(results.results);
+      setLoading(false);
+    };
+    fetchImagesData();
+  }, [cityInfo]);
+
+  console.log('images', images);
 
   if (loading) return <p>Loading...</p>;
   return (
     <>
       {/* {loading && <p>Loading...</p>} */}
-      <h2>
-        {cityInfo.city}, {cityInfo.region}
-      </h2>
-      <p>{cityInfo.country}</p>
-      <p>Population: {cityInfo.population}</p>
-      <button onClick={() => saveHandler()}>Save this trip</button>
+      <div>
+        <h2>
+          {cityInfo.city}, {cityInfo.region}
+        </h2>
+        <p>{cityInfo.country}</p>
+        <p>Population: {cityInfo.population}</p>
+        <button onClick={() => saveHandler()}>Save this trip</button>
+      </div>
+      <div className={Styles.cardList}>
+        {images.map((image) => (
+          <div className={Styles.card} key={image.id}>
+            <img
+              className={Styles.cardImage}
+              alt={image.alt_description}
+              src={image.urls.full}
+              width="50%"
+              height="50%"
+            ></img>
+          </div>
+        ))}
+      </div>
     </>
   );
 }
